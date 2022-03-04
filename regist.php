9e1d74af58954bc50b80b02cd57325c5fadc671a
DPI910Group5 <dpi910_group5@protonmail.com>
2022-03-04 16:24:48 -0500

    User Database

--- regist.php
+++ regist.php
@@ -1,3 +1,4 @@
+<?php include('server.php')?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,32 +13,32 @@
         <h1> Sign Up For DPI Account</h1>
       </div>
       <div class="nav_bar">
-        <a href="index.html" target="_blank">Home</a>
+        <a href="index.php" target="_blank">Home</a>
         <a href="login.php" target="_login">Login</a>
         <a href="about.html">About Us</a>
         <a href="contact.html">Contact Us</a>
       </div>
 
 
-<form name='registration' action="" method="POST" style="border:1px solid #ccc">
-    <div class="container">
+<form name='registration' action="regist.php" method="POST" style="border:1px solid #ccc">
+<?php include ('errors.php');?>    
+<div class="container">
    <b> <p>Kindly complete this form to create an account.</p> </b> 
-      <hr>
-  
+      <hr>  
       <label for="fname"><b>First Name</b></label>
-      <input type="text" placeholder="Enter First Name" name="fname" required>
+      <input type="text" placeholder="Enter First Name" name="fname" value="<?php echo $fname;?>" required>
 
       <label for="lname"><b>Last Name</b></label>
-      <input type="text" placeholder="Enter Last Name" name="lname" required>
+      <input type="text" placeholder="Enter Last Name" name="lname" value="<?php echo $lname;?>" required>
 
       <label for="email"><b>Email</b></label>
-      <input type="text" placeholder="Enter Email" name="email" required>
+      <input type="text" placeholder="Enter Email" name="email" value="<?php echo $email;?>" required>
   
       <label for="psw"><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="psw" required>
+      <input type="password" placeholder="Enter Password" name="psw" value="<?php echo $psw;?>" required>
   
       <label for="psw-repeat"><b>Repeat Password</b></label>
-      <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+      <input type="password" placeholder="Repeat Password" name="pswRepeat" value="<?php echo $pswRepeat;?>" required>
       
                    
       <div class="clearfix">
@@ -46,37 +47,5 @@
       </div>
     </div>
   </form>
-<?php  
-if(isset($_POST["submit"])){  
-if(!empty($_POST['fname']) && !empty($_POST['lname']) && !empty($_POST['email']) && !empty($_POST['psw'])) {  
-    $fname=$_POST['fname'];  
-    $lname=$_POST['lname']; 
-    $email=$_POST['email']; 
-    $pass=$_POST['psw'];  
-    $con=mysql_connect('localhost','root','') or die(mysql_error());  
-    mysql_select_db('dpi910') or die("cannot select DB");  
-  
-    $query=mysql_query("SELECT * FROM users WHERE fname='".$user."'");  
-    $numrows=mysql_num_rows($query);  
-    if($numrows==0)  
-    {  
-    $sql="INSERT INTO users(fname,lname,email,password) VALUES('$fname','$lname','$email','$pass')";  
-  
-    $result=mysql_query($sql);  
-        if($result){  
-    echo "Account Successfully Created";  
-    } else {  
-    echo "Failure!";  
-    }  
-  
-    } else {  
-    echo "That username already exists! Please try again with another.";  
-    }  
-  
-} else {  
-    echo "All fields are required!";  
-}  
-}  
-?> 
 </body>
 </html>
---
+++ server.php
@@ -0,0 +1,59 @@
+<?php
+session_start();
+
+// initializing variables
+$fname = "";
+$lname = "";
+$email    = "";
+$errors = array(); 
+
+// connect to the database
+$db = mysqli_connect('localhost', 'root', '', 'dpi910');
+
+// REGISTER USER
+if (isset($_POST['submit'])) {
+  // receive all input values from the form
+  $fname = mysqli_real_escape_string($db, $_POST['fname']);
+  $lname = mysqli_real_escape_string($db, $_POST['lname']);
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+  $password_1 = mysqli_real_escape_string($db, $_POST['psw']);
+  $password_2 = mysqli_real_escape_string($db, $_POST['pswRepeat']);
+
+  // form validation: ensure that the form is correctly filled ...
+  // by adding (array_push()) corresponding error unto $errors array
+  if (empty($fname)) { array_push($errors, "First name is required"); }
+  if (empty($lname)) { array_push($errors, "Last name is required"); }
+  if (empty($email)) { array_push($errors, "Email is required"); }
+  if (empty($password_1)) { array_push($errors, "Password is required"); }
+  if ($password_1 != $password_2) {
+	array_push($errors, "The two passwords do not match");
+  }
+
+  // first check the database to make sure 
+  // a user does not already exist with the same username and/or email
+  $user_check_query = "SELECT * FROM users WHERE fname='$fname' OR email='$email' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+  
+  if ($user) { // if user exists
+    if ($user['fname'] === $fname) {
+      array_push($errors, "First name already exists");
+    }
+
+    if ($user['email'] === $email) {
+      array_push($errors, "Email already exists");
+    }
+  }
+
+  // Finally, register user if there are no errors in the form
+  if (count($errors) == 0) {
+  	$password = md5($password_1);//encrypt the password before saving in the database
+
+  	$query = "INSERT INTO users (fname, lname, email, password) 
+  			  VALUES('$fname', '$lname', '$email', '$password')";
+  	mysqli_query($db, $query);
+  	$_SESSION['fname'] = $fname;
+  	$_SESSION['success'] = "You are now logged in";
+  	header('location: index.php');
+  }
+}
